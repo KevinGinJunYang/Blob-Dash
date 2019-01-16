4da9b89559ed8f87b71cd5fbbb6055888dcf0477
kevin <kevin7616@gmail.com>
2019-01-16 23:16:56 +1300

    finished game

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Instructions : MonoBehaviour {
+
+	public GameObject instructionsPanel;
+
+	public void openPanel(){
+		if (instructionsPanel != null) {
+			instructionsPanel.SetActive (true);
+		}
+
+	}
+
+	public void closePanel(){
+		instructionsPanel.SetActive (false);
+	}
+
+}
